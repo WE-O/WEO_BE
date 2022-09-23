b6b018ece6ee366d259b79422551aebc491c6af1
Song-Sujin <ssj1744@naver.com>
2022-09-23 21:26:02 +0900

    test: 레파지토리 변경

--- src/main/java/bside/weo/plant/entity/Hello.java
+++ src/main/java/bside/weo/plant/entity/Hello.java
@@ -4,8 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 @Getter @Setter
 public class Hello {
+
+    @Id @GeneratedValue
+    private Long id;
 }
