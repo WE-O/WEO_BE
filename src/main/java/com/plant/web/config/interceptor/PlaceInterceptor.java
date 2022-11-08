4a9f44731259eda04c0f4c50dea99e95ab5d1998
byeon.sungmun <201401339hs@gmail.com>
2022-11-08 16:06:29 +0900

    squash! fix :: 북마크 사용여부 적용

---
+++ src/main/java/com/plant/web/config/interceptor/PlaceInterceptor.java
@@ -0,0 +1,17 @@
+package com.plant.web.config.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@Component
+public class PlaceInterceptor implements HandlerInterceptor {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+}
