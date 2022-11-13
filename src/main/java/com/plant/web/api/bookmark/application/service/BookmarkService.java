8b09fa0e3cc06fe11a9c69367566224b4fb69c2a
Song-Sujin <ssj1744@naver.com>
2022-11-14 00:09:33 +0900

    fix :: import 수정

--- src/main/java/com/plant/web/api/bookmark/application/service/BookmarkService.java
+++ src/main/java/com/plant/web/api/bookmark/application/service/BookmarkService.java
@@ -14,8 +14,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Slf4j
